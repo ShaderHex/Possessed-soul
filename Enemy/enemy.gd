172908f6173c30d5be402b11532ee8175302bf1f
barkotbb <barkott9@gmail.com>
2025-05-12 23:09:11 +0300

    Added doors and other mechanisms

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,14 +2,15 @@ extends CharacterBody2D
 
 var is_possessed = false
 var player = null
+var original_modulate := Color(1, 1, 1)
+
+@onready var deadzone = $Deadzone
+@onready var player_node = $"../player"
+@onready var enemy_area = $EnemyArea
+@onready var flash_timer = $FlashTimer
 
 const SPEED = 200.0
 const GRAVITY = 980
-
-@onready var enemy_area = $EnemyArea
-
-func _ready():
-	print("Enemy script initialized")
 
 func _physics_process(delta):
 	if is_possessed:
@@ -25,15 +26,33 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 
 	move_and_slide()
-	
-
 
 func _on_enemy_area_entered(area: Area2D):
 	if area.is_in_group("player"):
 		player = area.get_parent()
-		print("Player entered enemy's area")
 
 func _on_enemy_area_exited(area: Area2D):
 	if area.get_parent() == player:
 		player = null
-		print("Player left enemy's area")
+
+func flash_white():
+	modulate = Color(1, 1, 1) * 2
+	flash_timer.start()
+
+func _on_flash_timer_timeout():
+	modulate = original_modulate
+
+func _on_deadzone_body_entered(body):
+	if body.name == "player" and not is_possessed:
+		player_node.player_health -= 10
+
+func _on_deadzone_body_exited(body):
+	if body.name == "player" and not is_possessed:
+		deadzone.monitoring = true
+
+func on_possess():
+	is_possessed = true
+	deadzone.monitoring = false
+
+func on_unpossess():
+	is_possessed = false
--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+@onready var player_health = 10
+
 var is_possessed = false
 var enemy = null
-var original_script = null
+var in_range_enemies = []
+var current_zoom_level = 1.0
+var zoom_factor = 1.1
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
@@ -10,21 +14,26 @@ const GRAVITY = 980
 
 @onready var player_area = $player_area
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var camera_2d = $Camera2D
+@onready var enemy_node = $"../Enemy"
 
 func _ready():
-	original_script = self.get_script()
 	if player_area:
 		player_area.connect("area_entered", _on_player_area_entered)
 		player_area.connect("area_exited", _on_player_area_exited)
-		print("Player script initialized")
+	print("Ready: Player initialized.")
+
+func _process(delta):
+	if is_possessed and enemy:
+		camera_2d.global_position = enemy.global_position
 	else:
-		printerr("PlayerArea node is missing!")
+		camera_2d.global_position = self.global_position
+		
+	if player_health <= 0:
+		print("Dead!")
 
 func _physics_process(delta):
-	if is_possessed:
-		if enemy == null:
-			print("Enemy is null in physics process while possessed")
-			return
+	if is_possessed and enemy:
 		if not enemy.is_on_floor():
 			enemy.velocity.y += GRAVITY * delta
 		if Input.is_action_just_pressed("ui_accept") and enemy.is_on_floor():
@@ -49,46 +58,66 @@ func _physics_process(delta):
 
 func _on_player_area_entered(area: Area2D):
 	var parent = area.get_parent()
-	print("Area entered:", area.name)
-	print("Area groups:", area.get_groups())
-	print("Parent node:", parent.name, "groups:", parent.get_groups())
-	
-	if parent.is_in_group("enemy"):
-		enemy = parent
-		print("Enemy detected:", enemy.name)
-	else:
-		print("Parent is not in 'enemy' group!")
-
+	if parent.is_in_group("enemy") and not in_range_enemies.has(parent):
+		in_range_enemies.append(parent)
+		print("Entered range of enemy: ", parent.name)
 
 func _on_player_area_exited(area: Area2D):
-	print("Area exited:", area.name, "for enemy:", enemy if enemy else "none")
-	if area.is_in_group("enemy") and area.get_parent() == enemy:
-		enemy = null
-		print("Enemy left player's area")
+	var parent = area.get_parent()
+	if parent in in_range_enemies:
+		in_range_enemies.erase(parent)
+		print("Exited range of enemy: ", parent.name)
 
 func _input(event):
 	if Input.is_action_just_pressed("possess"):
-		print("P pressed! Enemy is null:", enemy == null)
-		if !is_possessed:
-			if enemy != null:
-				print("Attempting possession...")
+		print("Possess key pressed")
+		if not is_possessed and in_range_enemies.size() > 0:
+			enemy = get_closest_enemy()
+			print("Attempting to possess closest enemy: ", enemy)
+			if enemy:
 				possess_enemy()
-		else:
+		elif is_possessed:
+			print("Unpossessing enemy.")
 			unpossess_enemy()
-		
+		else:
+			print("No enemies in range to possess.")
+
+func get_closest_enemy():
+	var closest = null
+	var closest_dist = INF
+	for e in in_range_enemies:
+		var dist = global_position.distance_to(e.global_position)
+		print("Checking enemy: ", e.name, " Distance: ", dist)
+		if dist < closest_dist:
+			closest = e
+			closest_dist = dist
+	return closest
 
 func possess_enemy():
 	if enemy:
-		print("Possessing enemy:", enemy.name)
+		print("Possessing enemy: ", enemy.name)
 		is_possessed = true
 		hide()
-		get_node("CollisionShape2D").disabled = true
+		collision_shape_2d.disabled = true
 		enemy.is_possessed = true
+		current_zoom_level += 0.1
+		camera_2d.zoom.x = current_zoom_level
+		camera_2d.zoom.y = current_zoom_level
+		enemy_node.get_node("Deadzone").monitoring = false
+		if enemy.has_method("flash_white"):
+			enemy.flash_white()
+		else:
+			print("Warning: Enemy has no flash_white method!")
 
 func unpossess_enemy():
 	if enemy:
+		print("Unpossessing enemy: ", enemy.name)
 		is_possessed = false
 		show()
-		get_node("CollisionShape2D").disabled = false
-		enemy.is_possessed = false
+		collision_shape_2d.disabled = false
+		self.global_position = enemy.global_position + Vector2(32, 0)
+		enemy.on_unpossess()
 		enemy = null
+		current_zoom_level -= 0.1
+		camera_2d.zoom.x = current_zoom_level
+		camera_2d.zoom.y = current_zoom_level
